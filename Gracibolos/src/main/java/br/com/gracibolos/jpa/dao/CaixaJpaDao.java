f9b2b34f2cd8ac0e509039396ebe27db09d7f491
RogerioHorauti <rogeriohorauti@gmail.com>
2017-06-12 08:36:46 -0300

    maven -> update project

---
+++ Gracibolos/src/main/java/br/com/gracibolos/jpa/dao/CaixaJpaDao.java
@@ -0,0 +1,40 @@
+package br.com.gracibolos.jpa.dao;
+
+import java.util.List;
+
+import br.com.gracibolos.jdbc.dao.GenericoDao;
+import br.com.gracibolos.jpa.model.Caixa;
+
+public class CaixaJpaDao implements GenericoDao<Caixa>{
+
+	@Override
+	public boolean inserir(Caixa caixa) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean alterar(Caixa caixa) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean excluir(Caixa caixa) throws Exception {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Caixa> listar() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Caixa> pesquisar(String pesquisa) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
